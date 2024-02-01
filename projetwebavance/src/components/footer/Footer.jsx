533aa117cc128473404b67e9d047bec3e936a425
AmelieRacine0 <141154177+AmelieRacine08@users.noreply.github.com>
2024-02-01 18:46:21 -0500

    Merge avec Imani

--- projetwebavance/src/components/footer/Footer.jsx
+++ projetwebavance/src/components/footer/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import Style from './footer.module.css'
+import Styles from "../navbar/navbar.module.css"
 
-const Footer = () => {
+/*const Footer = () => {
   return (
     <div className={Style.footer}>
 
-import Style from "../navbar/navbar.module.css"
 
+
+*/
 function Footer() {
   return (
     <div
-    className={Style.container}>
+    className={Styles.container}>
       &copy;Footer 
     </div>
   )
