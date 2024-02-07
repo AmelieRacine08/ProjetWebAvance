b96d47e93854c09d2ba875eee2b6d9928ea41574
mug-i <imanmugisha@gmail.com>
2024-02-06 19:31:00 -0500

    ajout de next/codemod

--- projetwebavance/src/app/page.js
+++ projetwebavance/src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import CartePourLien from "./cartePourLien/cartePourLien";
+import Styles from "./home.module.css";
 
 export default function Home() {
   return (
     <main>
       <Image src="/home1.jpg" alt="home" width={10000} height={250} />
       {/* <div>Home Page</div> */}
+      <div className={Styles.containerLien}>
+        <CartePourLien src="/event1.jpg" alt="event" name="event 1" />
+        <CartePourLien src="/event2.jpg" alt="event" name="event 2" />
+      </div>
     </main>
   );
 }
