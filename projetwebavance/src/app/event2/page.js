c184044324596eba5d2ccb0c0ceb8066f4e7a0ea
soumayachaouilbouziri <soumaiachaoui1@gmail.com>
2024-02-11 20:15:17 -0500

    modification de evenement 2

--- projetwebavance/src/app/event2/page.js
+++ projetwebavance/src/app/event2/page.js
@@ -5,9 +5,9 @@ const Event2 = () => {
   return (
     <>
       <TemplatePage 
-    title="Ottawa"
-    description="ottawa"
-    src="/FestivalBlues1.jpg"
+    title="Ottawa Jazz Festival"
+    description="Plongez dans l'ambiance envoûtante du Jazz Festival Ottawa 2024, une célébration captivante de la musique jazz dans la charmante capitale nationale. Rejoignez-nous pour une expérience musicale exaltante où les virtuoses du jazz et les nouveaux talents se réunissent pour enchanter vos sens et élever votre esprit. Venez vivre des moments inoubliables au cœur d'une atmosphère électrique et chaleureuse, où la passion et la créativité fusionnent pour créer des souvenirs musicaux qui dureront toute une vie."
+    src="/jazzFest.jpg"
     textButton1= "Plus d'information"
     textButton2="Contactez nous"
     />
