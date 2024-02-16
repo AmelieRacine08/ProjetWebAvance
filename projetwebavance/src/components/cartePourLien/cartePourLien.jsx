4bcae536a72db40e38f3cd86dc3eecf83cfb0e05
mug-i <imanmugisha@gmail.com>
2024-02-15 20:56:36 -0500

    modifications pour le css cartePourLien

--- projetwebavance/src/components/cartePourLien/cartePourLien.jsx
+++ projetwebavance/src/components/cartePourLien/cartePourLien.jsx
@@ -5,15 +5,15 @@ import Image from "next/image";
 const CartePourLien = (props) => {
   return (
     <a href={props.href} className={Styles.lien}>
-      <div className={Styles.imageContainer}>
-        <Image
-          src={props.src}
-          alt={props.alt}
-          width={350}
-          height={200}
-          className={Styles.customImage}
-        />
-      </div>
+      {/* <div className={Styles.imageContainer}> */}
+      <Image
+        src={props.src}
+        alt={props.alt}
+        width={1000}
+        height={1000}
+        className={Styles.customImage}
+      />
+      {/* </div> */}
       <div className={Styles.divName}>{props.name}</div>
     </a>
   );
